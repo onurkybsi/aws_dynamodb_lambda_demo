af546ee1734d4a1f39ff9560148dbe30393e68a2
Onur Kayabasi <onurbpm@outlook.com>
2022-11-29 17:02:16 +0100

    Deployment initialized

---
+++ deployment/lib/dynamodb-stack.ts
@@ -0,0 +1,25 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+
+export class DynamoDBStack extends Stack {
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
+
+		const coinPriceTable = new dynamodb.Table(this, "CoinPriceTable", {
+			tableName: "coin_price",
+			partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+			sortKey: { name: "price", type: dynamodb.AttributeType.STRING },
+			billingMode: dynamodb.BillingMode.PROVISIONED,
+			// One read capacity unit represents one strongly consistent read per second, or two eventually consistent reads per second, for an item up to 4 KB in size.
+			readCapacity: 3,
+			// One write capacity unit represents one write per second for an item up to 1 KB in size
+			writeCapacity: 3,
+		});
+		const readScaling = coinPriceTable.autoScaleReadCapacity({ minCapacity: 5, maxCapacity: 10 });
+		readScaling.scaleOnUtilization({ targetUtilizationPercent: 90 })
+
+		const writeScaling = coinPriceTable.autoScaleWriteCapacity({ minCapacity: 5, maxCapacity: 10 });
+		writeScaling.scaleOnUtilization({ targetUtilizationPercent: 90 })
+	}
+}
